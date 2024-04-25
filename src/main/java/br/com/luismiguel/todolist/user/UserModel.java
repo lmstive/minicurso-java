2dba71debcd0bb1f87259592106fd1b24ce83ea5
Luis Miguel <lmstive@gmail.com>
2024-04-25 17:18:22 -0300

    Criando tabela usuario

--- src/main/java/br/com/luismiguel/todolist/user/UserModel.java
+++ src/main/java/br/com/luismiguel/todolist/user/UserModel.java
@@ -1,10 +1,30 @@
 package br.com.luismiguel.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(name = "usuario")
     private String userName;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
