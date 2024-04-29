59d02f4bbd09267683a3dbe611a9c94ca9a880cc
Luis Miguel <lmstive@gmail.com>
2024-04-29 14:17:19 -0300

    Realizando filtro

---
+++ src/main/java/br/com/luismiguel/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,25 @@
+package br.com.luismiguel.todolist.filter;
+
+import java.io.IOException;
+
+import org.hibernate.annotations.Comment;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+@Component
+public class FilterTaskAuth implements Filter{
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+       
+        System.out.println("Chegou no filtro");        
+       chain.doFilter(request, response);
+    }
+
+}
