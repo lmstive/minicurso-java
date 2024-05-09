e3b05a0acd6e1b3712db151a5dcc21dc2e4d7e5e
Luis Miguel <lmstive@gmail.com>
2024-05-09 14:07:38 -0300

    Setando atributo no request

--- src/main/java/br/com/luismiguel/todolist/task/TaskController.java
+++ src/main/java/br/com/luismiguel/todolist/task/TaskController.java
@@ -1,11 +1,15 @@
 package br.com.luismiguel.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,8 +19,9 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
-        System.out.println("Chegou no controller");
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID)idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }
