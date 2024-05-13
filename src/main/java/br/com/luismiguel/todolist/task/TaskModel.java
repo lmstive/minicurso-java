bbf8f0b99377b7c202fc83bd5a04c77004247182
Luis Miguel <lmstive@gmail.com>
2024-05-13 15:17:33 -0300

    Try catch

--- src/main/java/br/com/luismiguel/todolist/task/TaskModel.java
+++ src/main/java/br/com/luismiguel/todolist/task/TaskModel.java
@@ -30,5 +30,11 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
    
+    public void setTitle(String title) throws Exception{
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter 50 caracteres");
+        }
+        this.title = title;
+    }
 
 }
